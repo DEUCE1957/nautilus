0db087ea1ebc63c8cf14b3a60ea9eacaad04aa6a
DEUCE1957 <2246306W@student.gla.ac.uk>
2021-02-02 13:07:40 +0000

    Add: Real Data Visualisation

--- Scripts/Extract_Simulation_Hyperparameters.py
+++ Scripts/Extract_Simulation_Hyperparameters.py
@@ -1,4 +1,4 @@
-import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ElementTree, XMLParser, TreeBuilder
 from pathlib import Path
 from Color import Color as C
 
@@ -17,16 +17,71 @@ with open(case_def, 'r') as file:
     xml = [line.strip("\n") for line in file.readlines() if not line.startswith("<!--")]
 print("\n".join(xml[0:5] + 
      [".".center(len(max(xml[0:5], key=len))) for i in range(3)] + [xml[-1], ""]))  
-root = ET.fromstring("\n".join(xml))
+parser = XMLParser() # Uses standard TreeBuilder
+parser.feed("\n".join(xml))
+root = parser.close()
+tree = ElementTree(element=root)
+print([item for item in tree.iterfind("constantsdef")])
 
-def walk_tree(root, depth=0):
+def prompt(k,v):
+    
+    return True if resp == "" else False
+    
+def print_tree(root, parent_path=".", record=False, depth=0):
     if depth == 0:
         print(f">> {C.BOLD}{C.RED}{root.tag}{C.END} <<")
         print(depth * "    " + f"{', '.join([f'{C.BOLD}{str(k).capitalize()}{C.END}: {v}' for k,v in root.attrib.items()])}")
     for child in root:
         print((depth+1)*"    " + f"{C.BOLD}{C.PURPLE if len(child) else C.DARKCYAN}{child.tag}{C.END} :: ", end="")
-        for k, v in child.attrib.items():
+        if record:
+            resp = input("\nPress enter to process contract for this tag, any other key to skip.")
+            skip = True if resp == "" else False
+        for i, (k, v) in enumerate(child.attrib.items()):
             print(f"{k}: {v}; ", end="")
+            with open(Path(__file__).parent / "HyperParameter_Contract.txt", "a+") as f:
+                if record and skip and k not in ["comment", "units_comment"]:
+                    if child.get("comment") and i == 0: print(child.get("comment"))
+                    if (resp := input("Keep this key?")) == "":
+                        if i == 0: f.write(f"{parent_path}/{child.tag}::")
+                        f.write(f"{k};")
+                if i == len(child.attrib.keys()) - 1: f.write("\n")
         print("")
-        walk_tree(child, depth=depth+1)
-walk_tree(root)
+        print_tree(child, parent_path=f"{parent_path}/{child.tag}", record=record and skip, depth=depth+1)
+
+def search_tree(root, tag, res=[]):
+    if root.tag == tag:
+        res.append(root)
+    for child in root:
+        new_match = search_tree(child, tag, res=res)
+        if res != new_match:
+            res = new_match
+            break
+    return res
+
+def swap_params(tree, SimParams):
+    for SimParam in SimParams:
+        print(SimParam)
+        elements = search_tree(tree.getroot(), SimParam.tag)
+        print("ELEMENTS", elements)
+        for element in elements:
+            print(f"ELEMENT: {element.tag} :: {element.attrib}")
+            if element.get(SimParam.key) is not None:
+                print(f"SET {SimParam.key} to {SimParam.value} (was {element.get(SimParam.key)})")
+                element.set(SimParam.key, SimParam.value)
+                
+class SimParam(object):
+    def __init__(self, tag, value, key="value"):
+        self.tag = tag
+        self.value = value
+        self.key = key
+
+    def __str__(self):
+        return f"Tag: '{self.tag}' ({type(self.tag)}), Value: {self.value} ({type(self.value)}), Key: {self.key} ({type(self.key)})"
+
+print("\n TREE BEFORE SWAP")
+print_tree(tree.getroot(), record=False)     
+test_param = SimParam("velocity", 2.0, key="v")
+print("Result:", search_tree(tree.getroot(), "velocity"))
+swap_params(tree, [test_param])
+print("\n\n TREE AFTER SWAP")
+print_tree(tree.getroot(), record=False)
---
+++ Scripts/Real_Data_Visualisation.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from pathlib import Path
+data_dir = Path(__file__).parent / "Real_Data"
+dfs = {}
+for file in data_dir.glob("*.txt"):
+    dfs[file.name] = (pd.read_csv(file, sep="	"))
+fig, axes = plt.subplots(nrows=len(dfs), ncols=len(dfs[file.name].columns), figsize=(4*len(dfs), 3*len(dfs[file.name].columns)))
+for i, (name, df) in enumerate(dfs.items()):
+    for j, col in enumerate(df.columns):
+        ax = axes[i][j]
+        ax.plot(df[col])
+        title = f"{name.split('.')[0]}\n{col}" if j == len(df.columns) // 2 else col
+        ax.set(title=title, xlabel="Step", ylabel=col)
+plt.tight_layout()
+plt.show()
+
