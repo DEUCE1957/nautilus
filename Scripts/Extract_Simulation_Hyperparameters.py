70a31ad88408215cf38f550f4f3f1ff4d249fe49
DEUCE1957 <2246306W@student.gla.ac.uk>
2021-02-21 12:53:02 +0000

    Doc: Add DocStrings

--- Scripts/Extract_Simulation_Hyperparameters.py
+++ Scripts/Extract_Simulation_Hyperparameters.py
@@ -1,9 +1,13 @@
 from xml.etree.ElementTree import ElementTree, XMLParser, TreeBuilder
 from collections import OrderedDict
 from pathlib import Path
+from shutil import copyfile, copytree
 from Color import Color as C
+from subprocess import Popen
+from datetime import datetime
 import re, numpy as np
 
+# >> Select Case <<
 cases_dir = Path(__file__).parent.parent / "Cases"
 case_names = {i: f.name for i, f in enumerate(cases_dir.glob("*")) if f.is_dir()}
 print("\n".join([f"{i}: {case_names[i]}" for i in range(len(case_names)) if i in case_names]))
@@ -14,6 +18,8 @@ while (resp := input("Please select a Case by number")):
             break
 case_path = cases_dir / case_name
 print(f"You selected the Case path: {case_path}")
+
+# >> Parse Case (Def)inition XML Tree <<<
 case_def = [f for f in case_path.glob("*.xml") if case_path.name in f.name][0]
 with open(case_def, 'r') as file:
     xml = [line.strip("\n") for line in file.readlines() if not line.startswith("<!--")]
@@ -23,28 +29,38 @@ parser = XMLParser() # Uses standard TreeBuilder
 parser.feed("\n".join(xml))
 root = parser.close()
 tree = ElementTree(element=root)
-print([item for item in tree.iterfind("constantsdef")])
+
+# >> Identify Experiment by Datetime <<
+timestamp = datetime.now().strftime("%d_%m_%Y_%Hh_%Mm")
+print(f"{C.BLUE}Timestamp{C.END}: {timestamp}")
 
 def is_number(s):
-    """ Returns True is string is a number. """
-    try:
-        float(s)
+    """ Returns True if string is a number. """
+    try: 
+        float(s) 
         return True
-    except ValueError:
+    except ValueError: 
         return False
     
 def print_tree(root, parent_path=".", record=False, depth=0):
-    if depth == 0:
+    """"Recursively print tree (left to right). 
+    record (Bool): Store nodes in 'HyperParameter_Contract.txt' [Default: False]
+    depth (int): Tracks level in tree, used for indentation [Default: 0]
+    parent_path (str): Tracks paths to nodes, used for direct access. [Default: '.']
+    """
+    if depth == 0: # Is Root
         print(f">> {C.BOLD}{C.RED}{root.tag}{C.END} <<")
         print(depth * "    " + f"{', '.join([f'{C.BOLD}{str(k).capitalize()}{C.END}: {v}' for k,v in root.attrib.items()])}")
+
     skip = True
-    for child in root:
-        print((depth+1)*"    " + f"{C.BOLD}{C.PURPLE if len(child) else C.DARKCYAN}{child.tag}{C.END} :: ", end="")
+    for child in root: 
+        # Print Child Tag in DarkCyan if LeafNode and Purple if internal node
+        print("    "*(depth+1) + f"{C.BOLD}{C.PURPLE if len(child) else C.DARKCYAN}{child.tag}{C.END} :: ", end="")
         if record:
             resp = input("\nPress enter to process contract for this tag, any other key to skip.")
-            skip = True if resp == "" else False
+            skip = True if resp == "" else False # Ensures contract is only overwritten if desired. 
         for i, (k, v) in enumerate(child.attrib.items()):
-            print(f"{k}: {v}; ", end="")
+            print(f"{k}: {v}; ", end="") # Attributes are concatenated to previous string 
             with open(Path(__file__).parent / "HyperParameter_Contract.txt", "a+") as f:
                 written = False
                 if record and skip:
@@ -54,11 +70,17 @@ def print_tree(root, parent_path=".", record=False, depth=0):
                         if (resp := input("Keep this key?")) == "":
                             written = True
                             f.write(f"{k}->{v};" if child.tag != "parameter" else f"{k}->{v}({child.get('key', 'name')});")
-                    if (i == len(child.attrib.keys()) - 1) and written: f.write("\n")
-        print("")
-        print_tree(child, parent_path=f"{parent_path}/{child.tag}", record=record and skip, depth=depth+1)
+                    if (i == len(child.attrib.keys()) - 1) and written:
+                        f.write("\n")
+        print("") # Newline for new child node
+        print_tree(child, parent_path=f"{parent_path}/{child.tag}",
+                   record=record and skip, depth=depth+1)
 
 def search_tree(root, tag, res=[]):
+    """Recursively finds all elements in Tree with provided tag.
+    root (node): Root of tree, must be iterable and have tag attribute.
+    tag (str): String identifier of node(s) we are searching for
+    res (list): Tracks matching nodes. [Default: []]"""
     if root.tag == tag:
         res.append(root)
     for child in root:
@@ -68,19 +90,11 @@ def search_tree(root, tag, res=[]):
             break
     return res
 
-
-# def swap_params(tree, SimParams):
-#     for SimParam in SimParams:
-#         print(SimParam)
-#         elements = search_tree(tree.getroot(), SimParam.tag)
-#         print("ELEMENTS", elements)
-#         for element in elements:
-#             print(f"ELEMENT: {element.tag} :: {element.attrib}")
-#             if element.get(SimParam.key) is not None:
-#                 print(f"SET {SimParam.key} to {SimParam.value} (was {element.get(SimParam.key)})")
-#                 element.set(SimParam.key, SimParam.value)
-
 def swap_params(root, params):
+    """Swaps list of Simulation parameters in tree, using full XML paths.
+    root (node): Root of tree, must be iterable.
+    params(list): List of SimParam objects
+    """
     for param, v in params.items():
         nodes = root.findall(param.id)
         if nodes == []: raise KeyError(f"XML id: '{param.id}' not found in Tree")
@@ -140,12 +154,35 @@ with open(Path(__file__).parent / "HyperParameter_Contract-ContinuousOnly.txt", 
             while params.get(param) is not None:
                 count += 1
                 param = SimParam(xpath, attr=attr, count=count, sec_key=sec_key)
-            params[param] = 99999 # v
+            params[param] = str(value)
             param_vector.append(float(value))
 
 print("\n".join([f"Simulation Parameter ({param}):  {v}" for param, v in params.items()]))
 print(param_vector)
 
+DurationNode = SimParam(id="./execution/parameters/parameter", attr="value", count=0, sec_key=("key", "TimeMax"))
+swap_params(tree, OrderedDict([(DurationNode, input("Duration in seconds"))]))
+TimeStepNode = SimParam(id="./execution/parameters/parameter", attr="value", count=0, sec_key=("key", "#DtMin"))
+InitialTimeStepNode = SimParam(id="./execution/parameters/parameter", attr="value", count=0, sec_key=("key", "#DtIni"))
+swap_params(tree, OrderedDict([(TimeStepNode, 1.0/120.0), (InitialTimeStepNode, 1.0/120.0)])) # 120 Hz
 swap_params(tree, params)
-print("\n\n TREE AFTER SWAP")
 print_tree(tree.getroot(), record=False)
+
+# >> Create backup of Case Definition file <<
+backup_path = case_def.parent / (case_name + "_Def_backup.xml")
+if not backup_path.exists():
+    copyfile(case_def, backup_path)
+# >> Overwrite existing case with swapped tree <<
+tree.write(case_def, method="xml")
+
+# >> Run Simulation (slow) <<
+batch_path = str(case_def.parent / (case_name + "_win64_GPU.bat"))
+print(f"Running Batch script: {batch_path}")
+p = Popen(batch_path, cwd=str(case_def.parent))
+stdout, stderr = p.communicate() # Will print to stdout
+
+print(f"{C.GREEN}{C.BOLD}Simulation Complete{C.END}")
+
+# >> Store MeasureTool output in this workspace <<
+copytree(case_def.parent / (case_name + "_out") / "measurements",
+         Path(__file__).parent / "Measurements" / (case_name + "_" + timestamp))
